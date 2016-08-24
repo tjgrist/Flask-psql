ce36e5adb71980823a713b5153b0e08b5177c2bd
roberttmoon <roberttmoon@gmail.com>
2016-08-24 13:59:24 -0500

    working skeleiton of flask psql test

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+#app.config.from_object(os.environ['APP_SETTINGS'])
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#db = SQLAlchemy(app)
 
-from models import *
+#from models import *
 
 @app.route('/')
 def index():
     return "Hello World!"
+
+from todo import mod
+app.register_blueprint(mod, url_prefix='/todo')
 
 if __name__ == '__main__':
     app.run()
