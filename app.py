fc0fe403e8c2dc03e1d5015d318dfe74a91f1143
TJ Grist <tjgrst@gmail.com>
2016-08-24 12:06:27 -0500

    heroku

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
-def hello():
+def index():
     return "Hello World!"
 
 if __name__ == '__main__':
